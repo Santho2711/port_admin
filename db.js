20cf66651633367a2c40aec3622b9c53a0bd797a
Santho2711 <santhoshsk27112001@gmail.com>
2025-06-03 20:28:47 +0530

    Data base changes

--- db.js
+++ db.js
@@ -2,8 +2,12 @@ const mongoose = require("mongoose");
 
 const ConnectDataBase = async () => {
   const dbName = "SanthoshAdmin";
+  const uri =
+    "mongodb+srv://santhosh2711kumar:<db_password>@santhoshadmin.3jims51.mongodb.net/?retryWrites=true&w=majority&appName=SanthoshAdmin";
   mongoose
-    .connect(`mongodb://127.0.0.1:27017/${dbName}`)
+    .connect(
+      `mongodb+srv://santhosh2711kumar:UZBpcDK6uNnDBOLF@santhoshadmin.3jims51.mongodb.net/?retryWrites=true&w=majority&appName=${dbName}`
+    )
     .then(() => console.log("Database connected"))
     .catch((e) => console.log(e, "Error in Database connection"));
 };
